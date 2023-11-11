0b0ad9ba15b670241b376452e45a9fde55a80f08
alex-toy <alexei.80@hotmail.fr>
2023-11-11 07:07:46 +0100

    sql app

---
+++ sqlapp/sqlapp/Services/CourseService.cs
@@ -0,0 +1,49 @@
+﻿using sqlapp.Models;
+using System.Globalization;
+
+namespace sqlapp.Services
+{
+    public class CourseService
+    {
+        private static string data_source = "files/data.csv";        
+
+        
+        public List<Course> GetCourses()
+        {
+            List<Course> _course_lst = new List<Course>();
+
+            StreamReader _reader = new StreamReader(File.OpenRead(data_source));
+            
+            while (!_reader.EndOfStream)
+            {
+                string _line = _reader.ReadLine();
+
+                string[] _values = _line.Split(',');
+
+                Course _course = new Course()
+                {
+                    CourseID = GetCourseId(_values),
+                    ExamImage = _values[1],
+                    CourseName = _values[2],
+                    Rating = GetRating(_values)
+                };
+
+                _course_lst.Add(_course);
+            }
+
+            return _course_lst;
+        }
+
+        private static decimal GetRating(string[] _values)
+        {
+            return decimal.Parse(_values[3], CultureInfo.InvariantCulture);
+        }
+
+        private static int GetCourseId(string[] _values)
+        {
+            int temp = int.Parse(_values[0]);
+            return temp;
+        }
+    }
+}
+
